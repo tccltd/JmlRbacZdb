c3eaf6a1203091ddd41d92afcd84f3768f0c7b6e
jmleroux <jmleroux.pro@gmail.com>
2014-05-10 18:05:43 +0200

    tests

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-namespace JmlRbacZdbTest;//Change this namespace for your test
+namespace JmlRbacZdbTest;
 
 use Zend\Loader\AutoloaderFactory;
 use Zend\Mvc\Service\ServiceManagerConfig;
@@ -8,7 +8,6 @@ use Zend\Stdlib\ArrayUtils;
 use RuntimeException;
 
 use ZfcRbac\Service\Rbac as RbacService;
-use ZfcRbacTest\RbacServiceTestCase;
 
 error_reporting(E_ALL | E_STRICT);
 chdir(__DIR__);
@@ -33,14 +32,16 @@ class Bootstrap
         if (isset($testConfig['module_listener_options']['module_paths'])) {
             $modulePaths = $testConfig['module_listener_options']['module_paths'];
             foreach ($modulePaths as $modulePath) {
-                if (($path = static::findParentPath($modulePath)) ) {
+                if (($path = static::findParentPath($modulePath))) {
                     $zf2ModulePaths[] = $path;
                 }
             }
         }
 
-        $zf2ModulePaths  = implode(PATH_SEPARATOR, $zf2ModulePaths) . PATH_SEPARATOR;
-        $zf2ModulePaths .= getenv('ZF2_MODULES_TEST_PATHS') ?: (defined('ZF2_MODULES_TEST_PATHS') ? ZF2_MODULES_TEST_PATHS : '');
+        $zf2ModulePaths = implode(PATH_SEPARATOR, $zf2ModulePaths) . PATH_SEPARATOR;
+        $zf2ModulePaths .= getenv('ZF2_MODULES_TEST_PATHS') ? : (defined(
+            'ZF2_MODULES_TEST_PATHS'
+        ) ? ZF2_MODULES_TEST_PATHS : '');
 
         static::initAutoloader();
 
@@ -78,24 +79,27 @@ class Bootstrap
         if (is_readable($vendorPath . '/autoload.php')) {
             $loader = include $vendorPath . '/autoload.php';
         } else {
-            $zf2Path = getenv('ZF2_PATH') ?: (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ? $vendorPath . '/ZF2/library' : false));
+            $zf2Path = getenv('ZF2_PATH') ? : (defined('ZF2_PATH') ? ZF2_PATH : (is_dir(
+                $vendorPath . '/ZF2/library'
+            ) ? $vendorPath . '/ZF2/library' : false));
 
             if (!$zf2Path) {
                 throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
             }
 
             include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
-
         }
 
-        AutoloaderFactory::factory(array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'autoregister_zf' => true,
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
+        AutoloaderFactory::factory(
+            array(
+                'Zend\Loader\StandardAutoloader' => array(
+                    'autoregister_zf' => true,
+                    'namespaces' => array(
+                        __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
+                    ),
                 ),
-            ),
-        ));
+            )
+        );
     }
 
     protected static function findParentPath($path)
@@ -104,7 +108,9 @@ class Bootstrap
         $previousDir = '.';
         while (!is_dir($dir . '/' . $path)) {
             $dir = dirname($dir);
-            if ($previousDir === $dir) return false;
+            if ($previousDir === $dir) {
+                return false;
+            }
             $previousDir = $dir;
         }
         return $dir . '/' . $path;
